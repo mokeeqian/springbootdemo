5886fd01123e85d107dd13e55f444bebbd6aba08
mokeeqian <mokeeqian@gmail.com>
2020-12-30 19:31:19 +0800

    add index.html

--- src/main/java/cn/edu/ahut/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/cn/edu/ahut/springbootdemo/SpringbootdemoApplication.java
@@ -2,8 +2,9 @@ package cn.edu.ahut.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class SpringbootdemoApplication {
 
     public static void main(String[] args) {
